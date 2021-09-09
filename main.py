5247d6c65b48f1b444d7268b3ffe7775e0ce8414
Paulofalcao2002 <paulohenriquemoreirafalcao@gmail.com>
2021-09-09 19:55:46 -0300

    Mostra sudoku formatado no terminal Detecta se o valor do número que será colocado no espaço vazio é valido

--- main.py
+++ main.py
@@ -27,6 +27,29 @@ def acha_zero(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if matrix[i][j] == 0:
-                return i, j #linha, coluna
+                return (i, j) #linha, coluna
+
+def numero_eh_valido(matrix, num, pos):
+    #linha
+    for i in range(len(matrix[0])):
+        if matrix[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    #coluna
+    for i in range(len(matrix)):
+        if matrix[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    #bloco 9x9
+    bloco_x = pos[1] // 3
+    bloco_y = pos[0] // 3
+
+    for i in range(bloco_y*3, bloco_y*3 + 3):
+        for j in range(bloco_x*3, bloco_x*3 + 3):
+            if matrix[i] == num and (i,j) != pos:
+                return False
+
+    return True
+
 
 mostra_sudoku(jogo)
